e4202cb44ccd7f3662fbbce5bd2b02931cd6a1a3
Administrator <hr.ahmadi689@yahoo.com>
2020-01-12 19:02:42 +0330

    thired

--- js/app.js
+++ js/app.js
@@ -1,31 +1,23 @@
 window.onload = init;
-var $output = document.getElementById('output');
-var $deput = document.getElementById('deput');
+    var $output = document.getElementById('output');
+    var $deput = document.getElementById('deput');
 
-function init() {
-    loadjson('GET', "http://localhost/crm/mysql.php?count&yesterday");
+    function init() {
+        loadjson('GET', "http://localhost/crm/mysql.php?count&yesterday");
 
-    function loadjson(m,u){
-        xhr = new XMLHttpRequest;
-        xhr.open(m,u);
-        xhr.onreadystatechange = function(){
-            if (this.readyState == 4 && this.status == 200) {
-                build(JSON.parse(xhr.response));
+        function loadjson(m,u){
+            xhr = new XMLHttpRequest;
+            xhr.open(m,u);
+            xhr.onreadystatechange = function(){
+                if (this.readyState == 4 && this.status == 200) {
+                    build(JSON.parse(xhr.response));
+                }
             }
+            xhr.send();
         }
-        xhr.send();
-    }
-    function  build(res) {
-        $output.innerText = res[0];
-        $deput.innerText = res[1];
-    }
-
-
-
-
-
-
-
-
+        function  build(res) {
+            $output.innerText = res[0];
+            $deput.innerText = res[1];
+        }
 
 }
